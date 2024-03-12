e7f96e90c06f8e0cd70a9599af51cac72db6b3c0
Karol Celeita <karol.celeitag@gmail.com>
2024-03-12 00:33:08 -0500

    Mejoras de diseño

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -47,7 +47,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # "crispy_forms",
+    # "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK =  "bootstrap5"
+
 WAGTAILSEARCH_BACKENDS = {
     'default':{
         'BACKEND':'wagtail.contrib.postgres_search.backend',
